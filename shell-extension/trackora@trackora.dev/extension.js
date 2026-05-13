f61c2537d201f2c61895d8f691b53a64ebdd2f40
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-05-13 19:43:51 +0530

    Create extension.js

---
+++ shell-extension/trackora@trackora.dev/extension.js
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/**
+ * Trackora GNOME Shell extension entry.
+ *
+ * Shell runs this code in-process with Mutter, so we can read Meta.Display’s
+ * focused window directly. Normal desktop apps cannot do this on Wayland.
+ */
+import GLib from 'gi://GLib';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+import {FocusLogPoller} from './lib/focusLogPoller.js';
+
+const INTERVAL_SEC = 3;
+
+export default class TrackoraExtension extends Extension {
+    enable() {
+        this._poller = new FocusLogPoller({intervalSec: INTERVAL_SEC});
+        this._poller.start();
+    }
+
+    disable() {
+        this._poller?.stop();
+        this._poller = null;
+    }
+}
