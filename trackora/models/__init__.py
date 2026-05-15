6a9cc2f0a1083e337352346a6ea38e64abdb67ef
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-05-15 23:01:19 +0530

    Create __init__.py

---
+++ trackora/models/__init__.py
@@ -0,0 +1,6 @@
+"""Dataclasses used by the Trackora tracking backend."""
+
+from trackora.models.session import ActiveSession
+from trackora.models.window_state import WindowState
+
+__all__ = ["ActiveSession", "WindowState"]
