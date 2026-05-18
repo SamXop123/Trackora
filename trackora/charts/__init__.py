9bf7782b20d1c9857cc56c9a841a6dc686b8df77
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-05-18 17:02:56 +0530

    Update __init__.py

--- trackora/charts/__init__.py
+++ trackora/charts/__init__.py
@@ -1,5 +1,6 @@
 """Chart widgets for the Trackora dashboard."""
 
 from trackora.charts.daily_usage_chart import DailyUsageChart
+from trackora.charts.weekly_usage_chart import WeeklyUsageChart
 
-__all__ = ["DailyUsageChart"]
+__all__ = ["DailyUsageChart", "WeeklyUsageChart"]
