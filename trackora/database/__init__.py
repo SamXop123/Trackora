f3b9425c35502baec5175e7e8d1e4bf8ea0c37f4
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-05-18 15:50:29 +0530

    Update __init__.py

--- trackora/database/__init__.py
+++ trackora/database/__init__.py
@@ -1,5 +1,6 @@
 """SQLite persistence for Trackora session tracking."""
 
+from trackora.database.dashboard import DashboardRepository
 from trackora.database.sqlite import SQLiteSessionStore
 
-__all__ = ["SQLiteSessionStore"]
+__all__ = ["DashboardRepository", "SQLiteSessionStore"]
