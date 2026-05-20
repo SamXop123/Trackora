285fbcff7554ff8ebba1502531a1fbfd284d4431
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-05-20 17:14:36 +0530

    Create __init__.py

---
+++ trackora/gui/pages/__init__.py
@@ -0,0 +1,19 @@
+"""Trackora dashboard page modules."""
+
+from trackora.gui.pages.dashboard import DashboardPage
+from trackora.gui.pages.timeline import TimelinePage
+from trackora.gui.pages.applications import ApplicationsPage
+from trackora.gui.pages.insights import InsightsPage
+from trackora.gui.pages.goals import GoalsPage
+from trackora.gui.pages.reports import ReportsPage
+from trackora.gui.pages.settings import SettingsPage
+
+__all__ = [
+    "DashboardPage",
+    "TimelinePage",
+    "ApplicationsPage",
+    "InsightsPage",
+    "GoalsPage",
+    "ReportsPage",
+    "SettingsPage",
+]
